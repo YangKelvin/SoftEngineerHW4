6659bef73a93b3cc4b5093207d3594e473301bc0
kelvin_yang <kelvin_yang@gss.com.tw>
2019-12-25 04:23:49 +0800

    Add MainTest.java

---
+++ src/com/test/MainTest.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import com.company.Main;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        String data = "80";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Main.main(new String[0]);
+        Main.main(new String[0]);
+    }
+}
